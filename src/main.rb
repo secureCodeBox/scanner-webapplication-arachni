8d09baf33a4b565847e2cfa403ca86c5e4886902
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2018-07-26 15:00:50 +0200

    Properly failing healthcheck when tcp connection to scanner cannot be established

--- src/main.rb
+++ src/main.rb
@@ -56,14 +56,18 @@ def healthcheck(connection)
 end
 
 def scanner_test
-  response = RestClient::Request.execute(
-    method: :get,
-    url: 'http://127.0.0.1:7331/scans',
-    timeout: 2
-  )
-  if response.code == 200
-    "SUCCESSFUL"
-  else
+  begin
+    response = RestClient::Request.execute(
+        method: :get,
+        url: 'http://127.0.0.1:7331/scans',
+        timeout: 2
+    )
+    if response.code == 200
+      "SUCCESSFUL"
+    else
+      "FAILED"
+    end
+  rescue
     "FAILED"
   end
 end
